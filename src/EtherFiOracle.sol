ff42f9ac6ecced6344a5763f866e171d7212225d
ReposCollector <seongyun@postech.ac.kr>
2024-02-19 13:34:14 +0900

    add unittest test_report_earlier_than_last_admin_execution_fails

--- src/EtherFiOracle.sol
+++ src/EtherFiOracle.sol
@@ -123,6 +123,7 @@ contract EtherFiOracle is Initializable, OwnableUpgradeable, PausableUpgradeable
         require(_isFinalized(slot), "Report Epoch is not finalized yet");
         require(computeSlotAtTimestamp(block.timestamp) >= reportStartSlot, "Report Slot has not started yet");
         require(lastPublishedReportRefSlot == etherFiAdmin.lastHandledReportRefSlot(), "Last published report is not handled yet");
+
         return slot > committeeMemberStates[_member].lastReportRefSlot;
     }
 
@@ -134,6 +135,7 @@ contract EtherFiOracle is Initializable, OwnableUpgradeable, PausableUpgradeable
         require(_report.refSlotTo == slotTo, "Report is for wrong slotTo");
         require(_report.refBlockFrom == blockFrom, "Report is for wrong blockFrom");
         require(_report.refBlockTo < block.number, "Report is for wrong blockTo");
+        require(_report.refBlockTo > etherFiAdmin.lastAdminExecutionBlock(), "Report must be based on the block after when the last admin execution is performed");
 
         // If two epochs in a row are justified, the current_epoch - 2 is considered finalized
         // Put 1 epoch more as a safe buffer
