b85150a9b020daadc456e24c3d68a33d5e054729
dave <dave.alexander@hey.com>
2024-02-20 12:23:08 -0700

    break requirement on 0.8.13 to enable 0.8.24 solidity features required for protocol v3

--- src/EtherFiNodesManager.sol
+++ src/EtherFiNodesManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
--- src/StakingManager.sol
+++ src/StakingManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
 import "./interfaces/ITNFT.sol";
 import "./interfaces/IBNFT.sol";
