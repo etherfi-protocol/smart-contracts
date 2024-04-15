42b6fbab535cf9792aca5e735117475754d17f93
ReposCollector <seongyun@postech.ac.kr>
2024-04-15 11:35:44 +0900

    add unit tests for BLAST

--- test/L2s.t.sol
+++ test/L2s.t.sol
@@ -47,6 +47,10 @@ interface IEtherfiL1SyncPoolETH {
         bytes calldata extraData
     ) external payable;
 
+    function onMessageReceived(uint32 originEid, bytes32 guid, address tokenIn, uint256 amountIn, uint256 amountOut)
+        external
+        payable;
+
     function endpoint() external view returns (address);
 }
 
@@ -87,19 +91,34 @@ interface ILayerZeroEndpointV2 {
 
 
 contract L2sTest is TestSetup {
+    struct ConfigPerL2 {
+        uint32 l2Eid;
+        address l2Oft;
+        address l2SyncPool;
+        address l1dummyToken;
+        address l1Receiver;
+    }
 
-    IEtherfiL1SyncPoolETH l1SyncPool;
-
+    IEtherfiL1SyncPoolETH l1SyncPool = IEtherfiL1SyncPoolETH(0xD789870beA40D056A4d26055d0bEFcC8755DA146);
     address l1OftAdapter = 0xFE7fe01F8B9A76803aF3750144C2715D9bcf7D0D;
     address l1Endpoint = 0x1a44076050125825900e736c501f859c50fE728c;
+    address ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    ConfigPerL2 BLAST = ConfigPerL2({
+        l2Eid: 30243,
+        l2Oft: 0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A,
+        l2SyncPool: 0x52c4221Cb805479954CDE5accfF8C4DcaF96623B,
+        l1dummyToken: 0x83998e169026136760bE6AF93e776C2F352D4b28,
+        l1Receiver: 0x27e120C518a339c3d8b665E56c4503DF785985c2
+    });
 
     function setUp() public {
         initializeRealisticFork(MAINNET_FORK);
 
-        l1SyncPool = IEtherfiL1SyncPoolETH(0xD789870beA40D056A4d26055d0bEFcC8755DA146);
-        l1OftAdapter = 0x361a67a81A4694612004fA79E23EA8e307d10289;
-
         _perform_etherfi_upgrade();
+
+        assertEq(l1SyncPool.endpoint(), l1Endpoint);
+        ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(address(l1SyncPool.endpoint()));
     }
 
 
@@ -109,33 +128,68 @@ contract L2sTest is TestSetup {
         assertEq(liquifierInstance.l1SyncPool(), address(l1SyncPool));
     }
 
-    function test_lzReceive_1() public {
-        uint32 l2Eid = 30243; // BLAST
-        address l2Oft = 0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A;
-        address l2SyncPool = 0x52c4221Cb805479954CDE5accfF8C4DcaF96623B;
-        address l1dummyToken = 0x83998e169026136760bE6AF93e776C2F352D4b28;
-        address l1Receiver = 0x27e120C518a339c3d8b665E56c4503DF785985c2;
+    function test_BLAST_fast_sync() public {
+        uint256 amountIn = 1e18;
+        uint256 amountOut = 0.9e18;
+
+        _test_fast_sync(BLAST.l2Eid, BLAST.l2Oft, BLAST.l2SyncPool, BLAST.l1dummyToken, BLAST.l1Receiver, amountIn, amountOut);
+    }
+
+    function test_BLAST_slow_sync() public {
+        // 'amountOut' is less than the actual weETH amount that can be minted with 'amountIn' ETH
+        // so the diff is considered as a fee and stay in the syncpool
+        uint256 amountIn = 1e18;
+        uint256 amountOut = 0.9e18;
+
+        uint256 liquifier_eth_balance = address(liquifierInstance).balance;
+        uint256 liquifier_dummy_balance = IDummyToken(BLAST.l1dummyToken).balanceOf(address(liquifierInstance));
+        uint256 lockbox_balance = weEthInstance.balanceOf(address(l1OftAdapter));
+        uint256 actualAmountOut = _sharesForDepositAmount(amountIn);
+
+        _test_fast_sync(BLAST.l2Eid, BLAST.l2Oft, BLAST.l2SyncPool, BLAST.l1dummyToken, BLAST.l1Receiver, amountIn, amountOut);
+
+        assertLt(amountOut, actualAmountOut);
+        assertEq(address(liquifierInstance).balance, liquifier_eth_balance);
+        assertEq(IDummyToken(BLAST.l1dummyToken).balanceOf(address(liquifierInstance)), liquifier_dummy_balance + amountIn);
+        assertEq(weEthInstance.balanceOf(address(l1OftAdapter)), lockbox_balance + amountOut);
         
+        _test_slow_sync(BLAST.l2Eid, BLAST.l1Receiver, amountIn);
+
+        assertEq(address(liquifierInstance).balance, liquifier_eth_balance + amountIn);
+        assertEq(IDummyToken(BLAST.l1dummyToken).balanceOf(address(liquifierInstance)), liquifier_dummy_balance);
+        assertEq(weEthInstance.balanceOf(address(l1OftAdapter)), lockbox_balance + amountOut);
+    }
+
+    // Slow Sync with the ETH bridged down to the L1
+    // - transfer the `amountIn` dummyETH
+    function _test_slow_sync(uint32 l2Eid, address receiver, uint256 amountIn) internal { 
+        vm.deal(receiver, amountIn);
+
+        vm.prank(receiver);
+        l1SyncPool.onMessageReceived{value: amountIn}(l2Eid, 0, ETH_ADDRESS, amountIn, 0);
+    }
+
+    // Fast Sync for native minting (input:`amountIn` ETH, output: `amountOut` weETH) at Layer 2 of Eid = `l2Eid`
+    // - mint the `amountIn` amount of dummy token & transfer it to the Liquifier
+    // - mint the <`amountIn` amount of eETH token & wrap it to weETH & transfer min(weETH balance, owed amount) to the lockbox (= L1 OFT Adapter)
+    function _test_fast_sync(uint32 l2Eid, address l2Oft, address l2SyncPool, address l1dummyToken, address l1Receiver, uint256 amountIn, uint256 amountOut) internal {
         assertEq(address(l1SyncPool.getDummyToken(l2Eid)), l1dummyToken);
         assertEq(l1SyncPool.getReceiver(l2Eid), l1Receiver);
-        assertEq(l1SyncPool.endpoint(), l1Endpoint);
 
         IDummyToken dummyToken = IDummyToken(l1SyncPool.getDummyToken(l2Eid));
 
-        vm.prank(liquifierInstance.owner());
-        liquifierInstance.registerToken(address(dummyToken), address(0), true, 0, 1, 1, true);
-        // liquifierInstance.registerToken(address(dummyToken), address(0), true, 0, 50, 1000, true);
-
-        uint256 amountIn = 1e18;
-        uint256 amountOut = 0.9e18;
-        address ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
         bytes memory message = abi.encode(ETH_ADDRESS, amountIn, amountOut);
 
-        ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(address(l1SyncPool.endpoint()));
-
-        vm.prank(address(endpoint));
+        vm.prank(address(l1Endpoint));
         l1SyncPool.lzReceive(Origin(l2Eid, _toBytes32(l2SyncPool), 0), 0, message, address(0), "");
+    }
 
+    function _sharesForDepositAmount(uint256 _depositAmount) internal view returns (uint256) {
+        uint256 totalPooledEther = liquidityPoolInstance.getTotalPooledEther() - _depositAmount;
+        if (totalPooledEther == 0) {
+            return _depositAmount;
+        }
+        return (_depositAmount * eETHInstance.totalShares()) / totalPooledEther;
     }
 
     function _toBytes32(address addr) internal pure returns (bytes32) {
