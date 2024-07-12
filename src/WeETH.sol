34b29415931b2ca685f16ac9b1945d01e460d2b7
Vaibhav Valecha <vaibhav@ether.fi>
2024-07-12 12:39:44 -0500

    fix spacing

--- src/WeETH.sol
+++ src/WeETH.sol
@@ -45,7 +45,7 @@ contract WeETH is ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, ERC20Pe
     }
 
     /// @notice Wraps eEth
-/// @param _eETHAmount the amount of eEth to wrap
+    /// @param _eETHAmount the amount of eEth to wrap
     /// @return returns the amount of weEth the user receives
     function wrap(uint256 _eETHAmount) public returns (uint256) {
         require(_eETHAmount > 0, "weETH: can't wrap zero eETH");
