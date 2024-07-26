a6beb9a7129caa6ba516a11322cca3ca672f7c44
Jacob Firek <firekjt@gmail.com>
2024-07-26 11:41:28 -0500

    spacing fix

--- src/WeETH.sol
+++ src/WeETH.sol
@@ -51,7 +51,7 @@ contract WeETH is ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, ERC20Pe
 
     /// @notice Wraps eEth
     /// @param _eETHAmount the amount of eEth to wrap
-/// @return returns the amount of weEth the user receives
+    /// @return returns the amount of weEth the user receives
     function wrap(uint256 _eETHAmount) public returns (uint256) {
         require(_eETHAmount > 0, "weETH: can't wrap zero eETH");
         uint256 weEthAmount = liquidityPool.sharesForAmount(_eETHAmount);
