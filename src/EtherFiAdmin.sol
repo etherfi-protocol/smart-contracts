7f228c7910df83672b79e87f6bec7be90947e8a3
Vaibhav Valecha <vaibhav@ether.fi>
2024-07-28 15:25:59 -0500

    remove casting uint128

--- src/EtherFiAdmin.sol
+++ src/EtherFiAdmin.sol
@@ -181,11 +181,10 @@ contract EtherFiAdmin is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
     //protocol owns the eth that was distributed to NO and treasury in eigenpods and etherfinodes 
     function _handleProtocolFees(IEtherFiOracle.OracleReport calldata _report) internal { 
-        require(_report.protocolFees >= 0, "EtherFiAdmin: protocol fees can't be negative");
         if(_report.protocolFees == 0) {
             return;
         }
-        liquidityPool.payProtocolFees(uint128(_report.protocolFees));
+        liquidityPool.payProtocolFees(_report.protocolFees);
     }
 
     function _handleAccruedRewards(IEtherFiOracle.OracleReport calldata _report) internal {
