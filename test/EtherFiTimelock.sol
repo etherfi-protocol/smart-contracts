962afe3d7ad79eadedb7697e6de92db9847e4163
ReposCollector <seongyun@postech.ac.kr>
2024-10-17 13:18:06 +0900

    add timelock txns

--- test/EtherFiTimelock.sol
+++ test/EtherFiTimelock.sol
@@ -318,9 +318,17 @@ contract TimelockTest is TestSetup {
         initializeRealisticFork(MAINNET_FORK);
         address target = address(managerInstance);
         bytes4 selector = 0x3ccc861d;
-
         bytes memory data = abi.encodeWithSelector(EtherFiNodesManager.updateAllowedForwardedExternalCalls.selector, selector, 0x7750d328b314EfFa365A0402CcfD489B80B0adda, true);
         _execute_timelock(target, data, true, true, true, true);
+    }
+
+    function test_upgrade_liquifier() public {
+        initializeRealisticFork(MAINNET_FORK);
+        {
+            address target = address(liquifierInstance);
+            bytes memory data = abi.encodeWithSelector(UUPSUpgradeable.upgradeTo.selector, 0x6B6D4e2DFcB864c83E29641429C528e8016BaCDf);
+            _execute_timelock(target, data, true, true, true, true);
+        }
     }
 }
 
