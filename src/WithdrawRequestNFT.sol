37306631ba3cb8477ef584c9574f65c470e3451a
ReposCollector <seongyun@postech.ac.kr>
2025-01-01 07:30:36 +0900

    initializeOnUpgrade cant be called twice

--- src/WithdrawRequestNFT.sol
+++ src/WithdrawRequestNFT.sol
@@ -72,7 +72,7 @@ contract WithdrawRequestNFT is ERC721Upgradeable, UUPSUpgradeable, OwnableUpgrad
     }
 
     function initializeOnUpgrade(address _pauser, uint16 _shareRemainderSplitToTreasuryInBps) external onlyOwner {
-        require(pauser == address(0), "Already initialized");
+        require(_currentRequestIdToScanFromForShareRemainder == 0, "Already initialized");
 
         paused = false;
         pauser = _pauser;
