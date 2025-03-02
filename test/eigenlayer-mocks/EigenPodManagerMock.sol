09991fcd08288fa7a6334715b859ab1a88e1c0fc
dave <dave.alexander@hey.com>
2025-03-02 15:52:33 -0700

    start updating mocks

--- test/eigenlayer-mocks/EigenPodManagerMock.sol
+++ test/eigenlayer-mocks/EigenPodManagerMock.sol
@@ -3,96 +3,77 @@ pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
 import "../../src/eigenlayer-interfaces/IEigenPodManager.sol";
+import "../../src/eigenlayer-interfaces/ISlasher.sol";
+import "../../src/eigenlayer-interfaces/IBeaconChainOracle.sol";
+import "../../src/eigenlayer-interfaces/IPausable.sol";
 
-contract EigenPodManagerMock is IEigenPodManager, Test {
-    IStrategy public constant beaconChainETHStrategy = IStrategy(0xbeaC0eeEeeeeEEeEeEEEEeeEEeEeeeEeeEEBEaC0);
-    IBeacon public eigenPodBeacon;
-    IETHPOSDeposit public ethPOS;
+contract EigenPodManagerMock is Test {
+    receive() external payable {}
+    fallback() external payable {}
 
-    address public owner;
+    mapping(address => int256) public podOwnerDepositShares;
 
-    mapping(address => int256) public podShares;
+    mapping(address => uint256) public podOwnerSharesWithdrawn;
 
-    constructor() {
-        owner = msg.sender;
+    struct BeaconChainSlashingFactor {
+        bool isSet;
+        uint64 slashingFactor;
     }
 
-    function slasher() external view returns(ISlasher) {}
+    mapping(address => BeaconChainSlashingFactor) _beaconChainSlashingFactor;
 
-    function createPod() external returns(address) {}
-
-    function stake(bytes calldata /*pubkey*/, bytes calldata /*signature*/, bytes32 /*depositDataRoot*/) external payable {}
-
-    function recordBeaconChainETHBalanceUpdate(address /*podOwner*/, int256 /*sharesDelta*/) external pure {}
-    
-    function updateBeaconChainOracle(IBeaconChainOracle /*newBeaconChainOracle*/) external pure {}
-
-    function ownerToPod(address /*podOwner*/) external pure returns(IEigenPod) {
-        return IEigenPod(address(0));
+    constructor(IPauserRegistry _pauserRegistry) {
     }
-
-    function getPod(address podOwner) external pure returns(IEigenPod) {
-        return IEigenPod(podOwner);
-    }
-
-    function beaconChainOracle() external pure returns(IBeaconChainOracle) {
-        return IBeaconChainOracle(address(0));
-    }   
-
-    function getBlockRootAtTimestamp(uint64 /*timestamp*/) external pure returns(bytes32) {
-        return bytes32(0);
-    }
-
-    function strategyManager() external pure returns(IStrategyManager) {
-        return IStrategyManager(address(0));
-    }
-    
-    function hasPod(address /*podOwner*/) external pure returns (bool) {
-        return false;
-    }
-
-    function pause(uint256 /*newPausedStatus*/) external{}
-
-    function pauseAll() external{}
-
-    function paused() external pure returns (uint256) {
-        return 0;
-    }
-    
-    function paused(uint8 /*index*/) external pure returns (bool) {
-        return false;
-    }
-
-    function setPauserRegistry(IPauserRegistry /*newPauserRegistry*/) external {}
-
-    function pauserRegistry() external pure returns (IPauserRegistry) {
-        return IPauserRegistry(address(0));
-    }
-
-    function unpause(uint256 /*newPausedStatus*/) external{}
 
     function podOwnerShares(address podOwner) external view returns (int256) {
-        return podShares[podOwner];
+        return podOwnerDepositShares[podOwner];
     }
+
+    function stakerDepositShares(address user, address) public view returns (uint256 depositShares) {
+        return podOwnerDepositShares[user] < 0 ? 0 : uint256(podOwnerDepositShares[user]);
+    } 
 
     function setPodOwnerShares(address podOwner, int256 shares) external {
-        podShares[podOwner] = shares;
+        podOwnerDepositShares[podOwner] = shares;
     }
 
-    function addShares(address /*podOwner*/, uint256 shares) external pure returns (uint256) {
-        // this is the "increase in delegateable tokens"
-        return (shares);
+    function addShares(
+        address podOwner,
+        IStrategy,
+        uint256 shares
+    ) external returns (uint256, uint256) {
+        uint256 existingDepositShares = uint256(podOwnerDepositShares[podOwner]);
+        podOwnerDepositShares[podOwner] += int256(shares);
+        return (existingDepositShares, shares);
     }
 
-    function withdrawSharesAsTokens(address podOwner, address destination, uint256 shares) external {}
-
-    function removeShares(address podOwner, uint256 shares) external {}
-
-    function numPods() external view returns (uint256) {}
+    function removeDepositShares(
+        address podOwner, 
+        IStrategy, // strategy 
+        uint256 shares
+    ) external returns (uint256) {
+        int256 updatedShares = podOwnerDepositShares[podOwner] - int256(shares);
+        podOwnerDepositShares[podOwner] = updatedShares;
+        return uint256(updatedShares);
+    }
 
     function denebForkTimestamp() external pure returns (uint64) {
         return type(uint64).max;
     }
 
-    function setDenebForkTimestamp(uint64 timestamp) external{}
+    function withdrawSharesAsTokens(address podOwner, address /** strategy */, address /** token */, uint256 shares) external {
+        podOwnerSharesWithdrawn[podOwner] += shares;
+    }
+
+    function setBeaconChainSlashingFactor(address staker, uint64 bcsf) external {
+        _beaconChainSlashingFactor[staker] = BeaconChainSlashingFactor({
+            isSet: true,
+            slashingFactor: bcsf
+        });
+    }
+
+    function beaconChainSlashingFactor(address staker) external view returns (uint64) {
+        BeaconChainSlashingFactor memory bsf = _beaconChainSlashingFactor[staker];
+        return bsf.isSet ? bsf.slashingFactor : WAD;
+    }
 }
