4b66bed273b34d262596be62b5bd81335b4e00cd
Pankaj Jagtap <jagtap.pankaj2000@gmail.com>
2025-10-08 19:01:29 -0500

    feat: Update stETH withdrawal transaction script with new functionality
    - Enhanced `scheduleCleanUpStorageOnEFRM` and `_clearOutSlotForUpgrade` functions to utilize operation IDs for better transaction tracking.
    - Implemented `initializeTokenParametersEFRM` function to set token parameters for `EtherFiRedemptionManager`.

--- script/stETH-withdrawals/transactions.s.sol
+++ script/stETH-withdrawals/transactions.s.sol
@@ -6,14 +6,15 @@ import "forge-std/console2.sol";
 import {Utils} from "../utils/utils.sol";
 import {EtherFiTimelock} from "../../src/EtherFiTimelock.sol";
 import {UUPSUpgradeable} from "lib/openzeppelin-contracts/contracts/proxy/utils/UUPSUpgradeable.sol";
-import {EtherFiRedemptionManager} from "../../src/EtherFiRedemptionManager.sol";
 import {EtherFiRedemptionManagerTemp} from "../../src/EtherFiRedemptionManagerTemp.sol";
 import {EtherFiRestaker} from "../../src/EtherFiRestaker.sol";
+import {EtherFiRedemptionManager} from "../../src/EtherFiRedemptionManager.sol";
 import {LiquidityPool} from "../../src/LiquidityPool.sol";
 
 contract StETHWithdrawalsTransactions is Script, Utils {
     EtherFiTimelock etherFiTimelock = EtherFiTimelock(payable(0x9f26d4C958fD811A1F59B01B86Be7dFFc9d20761));
     EtherFiTimelock etherfiOperatingTimelock = EtherFiTimelock(payable(0xcD425f44758a08BaAB3C4908f3e3dE5776e45d7a));
+    EtherFiRestaker etherFiRestakerInstance = EtherFiRestaker(payable(0x1B7a4C3797236A1C37f8741c0Be35c2c72736fFf));
     //--------------------------------------------------------------------------------------
     //--------------------- Previous Implementations ---------------------------------------
     //--------------------------------------------------------------------------------------
@@ -43,6 +44,7 @@ contract StETHWithdrawalsTransactions is Script, Utils {
     address constant etherFiRedemptionManager = 0xDadEf1fFBFeaAB4f68A9fD181395F68b4e4E7Ae0;
     address constant eETH = 0x35fA164735182de50811E8e2E824cFb9B6118ac2;
     address constant weETH = 0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee;
+    address constant stETH = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
     address constant liquidityPool = 0x308861A430be4cce5502d0A12724771Fc6DaF216;
     address constant operatingTimelock = 0xcD425f44758a08BaAB3C4908f3e3dE5776e45d7a;
     address constant roleRegistry = 0x62247D29B4B9BECf4BB73E0c722cf6445cfC7cE9;
@@ -68,31 +70,35 @@ contract StETHWithdrawalsTransactions is Script, Utils {
 
         vm.startBroadcast(TIMELOCK_CONTROLLER);
         // vm.startPrank(TIMELOCK_CONTROLLER);
-
         scheduleCleanUpStorageOnEFRM();
         upgrade();
-        rollbackUpgrade();
-
         // vm.stopPrank();
         vm.stopBroadcast();
 
         vm.startBroadcast(ETHERFI_OPERATING_ADMIN);
         // vm.startPrank(ETHERFI_OPERATING_ADMIN);
-
-        rollbackEFRMStorage();
-        
+        initializeTokenParametersEFRM();
         // vm.stopPrank();
         vm.stopBroadcast();
 
-        
+        // console2.log("=============== ROLLBACK TRANSACTIONS ================");
+        // console2.log("================================================");
+
+        // vm.startBroadcast(TIMELOCK_CONTROLLER);
+        // rollbackUpgrade();
+        // vm.stopBroadcast();
+
+        // vm.startBroadcast(ETHERFI_OPERATING_ADMIN);
+        // rollbackEFRMStorage();
+        // vm.stopBroadcast();
     }
 
     function scheduleCleanUpStorageOnEFRM() public {
-        _upgradeEFRMToTemp();
-        _clearOutSlotForUpgrade();
+        bytes32 firstTxId = _upgradeEFRMToTemp();
+        _clearOutSlotForUpgrade(firstTxId);
     }
 
-    function _upgradeEFRMToTemp() internal {
+    function _upgradeEFRMToTemp() internal returns (bytes32) {
         address[] memory targets = new address[](1);
         bytes[] memory data = new bytes[](1);
         uint256[] memory values = new uint256[](1);
@@ -101,6 +107,8 @@ contract StETHWithdrawalsTransactions is Script, Utils {
         data[0] = abi.encodeWithSelector(UUPSUpgradeable.upgradeTo.selector, etherFiRedemptionManagerTempImpl);
 
         bytes32 timelockSalt = keccak256(abi.encode(targets, data, block.number));
+        bytes32 operationId = etherFiTimelock.hashOperationBatch(targets, values, data, bytes32(0), timelockSalt);
+        
         bytes memory scheduleCalldata = abi.encodeWithSelector(
             etherFiTimelock.scheduleBatch.selector,
             targets,
@@ -135,10 +143,11 @@ contract StETHWithdrawalsTransactions is Script, Utils {
         console2.log("Current timestamp:", block.timestamp);
         etherFiTimelock.scheduleBatch(targets, values, data, bytes32(0), timelockSalt, MIN_DELAY_TIMELOCK);
         console2.log("Schedule of Upgrade EFRM To Temp successful");
+        console2.log("Operation ID:", vm.toString(operationId));
         console2.log("================================================");
         console2.log("");
 
-        console2.log("=== FAST FORWARDING TIME ===");
+        // console2.log("=== FAST FORWARDING TIME ===");
         // vm.warp(block.timestamp + MIN_DELAY_TIMELOCK + 1); // +1 to ensure it's past the delay
         // increaseTimeTenderly(MIN_DELAY_TIMELOCK + 1);
 
@@ -147,9 +156,11 @@ contract StETHWithdrawalsTransactions is Script, Utils {
         // console2.log("Execute of Upgrade EFRM To Temp successful");
         // console2.log("================================================");
         // console2.log("");
+        
+        return operationId;
     }
 
-    function _clearOutSlotForUpgrade() internal {
+    function _clearOutSlotForUpgrade(bytes32 predecessor) internal {
         address[] memory targets = new address[](1);
         bytes[] memory data = new bytes[](1);
         uint256[] memory values = new uint256[](1);
@@ -161,12 +172,14 @@ contract StETHWithdrawalsTransactions is Script, Utils {
         //------------------------------- SCHEDULE TX --------------------------------------
         //------------------------------------------------      --------------------------------------
         bytes32 timelockSalt = keccak256(abi.encode(targets, data, block.number));
+        bytes32 operationId = etherFiTimelock.hashOperationBatch(targets, values, data, predecessor, timelockSalt);
+        
         bytes memory scheduleCalldata = abi.encodeWithSelector(
             etherFiTimelock.scheduleBatch.selector,
             targets,
             values,
             data,
-            bytes32(0)/*=predecessor*/,
+            predecessor, // Use the predecessor from the first transaction
             timelockSalt,
             MIN_DELAY_TIMELOCK/*=minDelay*/
         );
@@ -182,7 +195,7 @@ contract StETHWithdrawalsTransactions is Script, Utils {
             targets,
             values,
             data,
-            bytes32(0)/*=predecessor*/,
+            predecessor, // Use the predecessor from the first transaction
             timelockSalt
         );
 
@@ -194,14 +207,93 @@ contract StETHWithdrawalsTransactions is Script, Utils {
         // uncomment to run against fork
         console2.log("=== SCHEDULING BATCH ===");
         console2.log("Current timestamp:", block.timestamp);
-        etherFiTimelock.scheduleBatch(targets, values, data, bytes32(0), timelockSalt, MIN_DELAY_TIMELOCK);
+        console2.log("Predecessor:", vm.toString(predecessor));
+        etherFiTimelock.scheduleBatch(targets, values, data, predecessor, timelockSalt, MIN_DELAY_TIMELOCK);
+        console2.log("Operation ID:", vm.toString(operationId));
+        console2.log("================================================");
+        console2.log("");
+        console2.log("scheduled clearOutSlotForUpgrade Tx:");
 
         // console2.log("=== FAST FORWARDING TIME ===");
         // vm.warp(block.timestamp + MIN_DELAY_TIMELOCK + 1); // +1 to ensure it's past the delay
         // increaseTimeTenderly(MIN_DELAY_TIMELOCK + 1);
 
         // console2.log("New timestamp:", block.timestamp);
+        etherFiTimelock.executeBatch(targets, values, data, bytes32(0), timelockSalt);
+    }
+
+    function initializeTokenParametersEFRM() public {
+        address[] memory targets = new address[](1);
+        bytes[] memory data = new bytes[](1);
+        uint256[] memory values = new uint256[](1);
+
+        address[] memory _tokens = new address[](2);
+        _tokens[0] = eETH;
+        _tokens[1] = stETH;
+        uint16[] memory _exitFeeSplitToTreasuryInBps = new uint16[](2);
+        _exitFeeSplitToTreasuryInBps[0] = 1000;
+        _exitFeeSplitToTreasuryInBps[1] = 1000;
+        uint16[] memory _exitFeeInBps = new uint16[](2);
+        _exitFeeInBps[0] = 30;
+        _exitFeeInBps[1] = 10;
+        uint16[] memory _lowWatermarkInBpsOfTvl = new uint16[](2);
+        _lowWatermarkInBpsOfTvl[0] = 100;
+        _lowWatermarkInBpsOfTvl[1] = 0;
+        uint256[] memory _bucketCapacity = new uint256[](2);
+        _bucketCapacity[0] = 2000000000;
+        _bucketCapacity[1] = 2000000000;
+        uint256[] memory _bucketRefillRate = new uint256[](2);
+        _bucketRefillRate[0] = 23148;
+        _bucketRefillRate[1] = 5000;
+        
+        targets[0] = address(etherFiRedemptionManager);
+        data[0] = abi.encodeWithSelector(EtherFiRedemptionManager.initializeTokenParameters.selector, _tokens, _exitFeeSplitToTreasuryInBps, _exitFeeInBps, _lowWatermarkInBpsOfTvl, _bucketCapacity, _bucketRefillRate);
+
+        bytes32 timelockSalt = keccak256(abi.encode(targets, data, block.number));
+        bytes memory scheduleCalldata = abi.encodeWithSelector(
+            etherfiOperatingTimelock.scheduleBatch.selector,
+            targets,
+            values,
+            data,
+            bytes32(0)/*=predecessor*/,
+            timelockSalt,
+            MIN_DELAY_OPERATING_TIMELOCK/*=minDelay*/
+        );
+
+        console2.log("Schedule Initialize Token Parameters EFRM Tx:");
+        console2.logBytes(scheduleCalldata);
+        console2.log("================================================");
+        console2.log("");
+
+        bytes memory executeCalldata = abi.encodeWithSelector(
+            etherfiOperatingTimelock.executeBatch.selector,
+            targets,
+            values,
+            data,
+            bytes32(0)/*=predecessor*/,
+            timelockSalt
+        );
+
+        console2.log("Execute Initialize Token Parameters EFRM Tx:");
+        console2.logBytes(executeCalldata);
+        console2.log("================================================");
+        console2.log("");
+
+        // uncomment to run against fork
+        console2.log("=== SCHEDULING BATCH ===");
+        etherfiOperatingTimelock.scheduleBatch(targets, values, data, bytes32(0), timelockSalt, MIN_DELAY_OPERATING_TIMELOCK);
+        console2.log("Schedule of Initialize Token Parameters EFRM Tx successful");
+        console2.log("================================================");
+        console2.log("");
+
+        // console2.log("=== FAST FORWARDING TIME ===");
+        // vm.warp(block.timestamp + MIN_DELAY_TIMELOCK + 1); // +1 to ensure it's past the delay
+        // increaseTimeTenderly(MIN_DELAY_TIMELOCK + 1);
+        // console2.log("New timestamp:", block.timestamp);
         // etherFiTimelock.executeBatch(targets, values, data, bytes32(0), timelockSalt);
+        // console2.log("Execute of Initialize Token Parameters EFRM Tx successful");
+        // console2.log("================================================");
+        // console2.log("");
     }
 
     function upgrade() public {
@@ -266,7 +358,6 @@ contract StETHWithdrawalsTransactions is Script, Utils {
         // console2.log("================================================");
         // console2.log("");
     }
-
 
     function rollbackUpgrade() public {
         address[] memory targets = new address[](4);
