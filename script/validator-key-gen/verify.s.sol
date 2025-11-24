7a2c50fb2e092cc044d8da59996536098d05aee0
Pankaj Jagtap <jagtap.pankaj2000@gmail.com>
2025-11-24 17:37:47 -0500

    feat: Enhance VerifyValidatorKeyGen script with EtherFiNodesManager and EtherFiRestaker address verifications

--- script/validator-key-gen/verify.s.sol
+++ script/validator-key-gen/verify.s.sol
@@ -7,6 +7,7 @@ import {LiquidityPool} from "../../src/LiquidityPool.sol";
 import {StakingManager} from "../../src/StakingManager.sol";
 import {EtherFiNodesManager} from "../../src/EtherFiNodesManager.sol";
 import {RoleRegistry} from "../../src/RoleRegistry.sol";
+import {EtherFiRestaker} from "../../src/EtherFiRestaker.sol";
 
 import {IEtherFiNodesManager} from "../../src/interfaces/IEtherFiNodesManager.sol";
 import {IStakingManager} from "../../src/interfaces/IStakingManager.sol";
@@ -30,24 +31,34 @@ contract VerifyValidatorKeyGen is Script {
     address constant STAKING_MANAGER_PROXY = 0x25e821b7197B146F7713C3b89B6A4D83516B912d;
     address constant LIQUIDITY_POOL_PROXY = 0x308861A430be4cce5502d0A12724771Fc6DaF216;
     address constant ETHERFI_NODES_MANAGER_PROXY = 0x8B71140AD2e5d1E7018d2a7f8a288BD3CD38916F;
+    address constant ETHERFI_RESTAKER_PROXY = 0x6fDF76c039654f46b9d7e851Fb8135569080C033;
+
     address constant AUCTION_MANAGER = 0x00C452aFFee3a17d9Cecc1Bcd2B8d5C7635C4CB9;
     address constant ETHERFI_NODE_BEACON = 0x3c55986Cfee455E2533F4D29006634EcF9B7c03F;
     address constant ROLE_REGISTRY = 0x62247D29B4B9BECf4BB73E0c722cf6445cfC7cE9;
     address constant ETH_DEPOSIT_CONTRACT = 0x00000000219ab540356cBB839Cbe05303d7705Fa;
+    address constant RATE_LIMITER_PROXY = 0x6C7c54cfC2225fA985cD25F04d923B93c60a02F8;
+    address constant REWARDS_COORDINATOR = 0x7750d328b314EfFa365A0402CcfD489B80B0adda;
 
     address constant ETHERFI_OPERATING_ADMIN = 0x2aCA71020De61bb532008049e1Bd41E451aE8AdC;
     address constant realElExiter = 0x12582A27E5e19492b4FcD194a60F8f5e1aa31B0F;
+
     // === IMPLEMENTATION ADDRESSES ===
     address constant LIQUIDITY_POOL_IMPL = 0x4C6767A0afDf06c55DAcb03cB26aaB34Eed281fc;
     LiquidityPool liquidityPool = LiquidityPool(payable(LIQUIDITY_POOL_PROXY));
     address constant STAKING_MANAGER_IMPL = 0xF73996bceDE56AD090024F2Fd4ca545A3D06c8E3;
     StakingManager stakingManager = StakingManager(STAKING_MANAGER_PROXY);
+    address constant ETHERFI_NODES_MANAGER_IMPL = 0x69B35625A66424cBA28bEd328E1CbFD239714cD7;
+    EtherFiNodesManager etherFiNodesManager = EtherFiNodesManager(ETHERFI_NODES_MANAGER_PROXY);
+    address constant ETHERFI_RESTAKER_IMPL = 0x6fDF76c039654f46b9d7e851Fb8135569080C033;
+    EtherFiRestaker constant etherFiRestaker = EtherFiRestaker(payable(ETHERFI_RESTAKER_PROXY));
 
-    EtherFiNodesManager constant etherFiNodesManager = EtherFiNodesManager(ETHERFI_NODES_MANAGER_PROXY);
     RoleRegistry constant roleRegistry = RoleRegistry(ROLE_REGISTRY);
 
     LiquidityPool constant liquidityPoolImplementation = LiquidityPool(payable(LIQUIDITY_POOL_IMPL));
     StakingManager constant stakingManagerImplementation = StakingManager(STAKING_MANAGER_IMPL);
+    EtherFiRestaker constant etherFiRestakerImplementation = EtherFiRestaker(payable(ETHERFI_RESTAKER_IMPL));
+    EtherFiNodesManager constant etherFiNodesManagerImplementation = EtherFiNodesManager(ETHERFI_NODES_MANAGER_IMPL);
 
     function run() public {
         console2.log("================================================");
@@ -65,14 +76,18 @@ contract VerifyValidatorKeyGen is Script {
     function verifyBytecode() internal {
         LiquidityPool newLiquidityPoolImplementation = new LiquidityPool();
         StakingManager newStakingManagerImplementation = new StakingManager(address(LIQUIDITY_POOL_PROXY), address(ETHERFI_NODES_MANAGER_PROXY), address(ETH_DEPOSIT_CONTRACT), address(AUCTION_MANAGER), address(ETHERFI_NODE_BEACON), address(ROLE_REGISTRY));
+        EtherFiNodesManager newEtherFiNodesManagerImplementation = new EtherFiNodesManager(address(STAKING_MANAGER_PROXY), address(ROLE_REGISTRY), address(RATE_LIMITER_PROXY));
+        EtherFiRestaker newEtherFiRestakerImplementation = new EtherFiRestaker(address(REWARDS_COORDINATOR));
 
         contractCodeChecker.verifyContractByteCodeMatch(LIQUIDITY_POOL_IMPL, address(newLiquidityPoolImplementation));
         contractCodeChecker.verifyContractByteCodeMatch(STAKING_MANAGER_IMPL, address(newStakingManagerImplementation));
+        contractCodeChecker.verifyContractByteCodeMatch(ETHERFI_NODES_MANAGER_IMPL, address(newEtherFiNodesManagerImplementation));
+        contractCodeChecker.verifyContractByteCodeMatch(ETHERFI_RESTAKER_IMPL, address(newEtherFiRestakerImplementation));
 
         console2.log(unicode"✓ Bytecode verified successfully");
     }
 
-    function verifyAddress() public view {
+    function verifyAddress() public {
         // LiquidityPool
         {
             bytes memory constructorArgs = abi.encode();
@@ -93,6 +108,28 @@ contract VerifyValidatorKeyGen is Script {
             );
             address predictedAddress = factory.computeAddress(commitHashSalt, bytecode);
             require(STAKING_MANAGER_IMPL == predictedAddress, "StakingManager deployment address mismatch");
+        }
+
+        // EtherFiNodesManager
+        {
+            bytes memory constructorArgs = abi.encode(address(STAKING_MANAGER_PROXY), address(ROLE_REGISTRY), address(RATE_LIMITER_PROXY));
+            bytes memory bytecode = abi.encodePacked(
+                type(EtherFiNodesManager).creationCode,
+                constructorArgs
+            );
+            address predictedAddress = factory.computeAddress(commitHashSalt, bytecode);
+            require(ETHERFI_NODES_MANAGER_IMPL == predictedAddress, "EtherFiNodesManager deployment address mismatch");
+        }
+
+        // EtherFiRestaker
+        {
+            bytes memory constructorArgs = abi.encode(REWARDS_COORDINATOR);
+            bytes memory bytecode = abi.encodePacked(
+                type(EtherFiRestaker).creationCode,
+                constructorArgs
+            );
+            address predictedAddress = factory.computeAddress(commitHashSalt, bytecode);
+            require(ETHERFI_RESTAKER_IMPL == predictedAddress, "EtherFiRestaker deployment address mismatch");
         }
 
         console2.log(unicode"✓ Address verified successfully");
