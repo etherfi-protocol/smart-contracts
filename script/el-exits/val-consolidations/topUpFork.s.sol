608f12e84ce1b27dd349b5727ffe48221d595441
pankajjagtapp <pankaj@ether.fi>
2025-12-16 13:28:34 -0500

    feat: Implement TopUpFork script for validator size adjustment and report submission
    - Added `TopUpFork` contract to facilitate validator size setting and report submission process.
    - Includes functionality for AVS Operators to submit reports and for the admin to execute tasks based on those reports.
    - Implements time advancement to ensure proper report epoch finalization.

---
+++ script/el-exits/val-consolidations/topUpFork.s.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "forge-std/StdJson.sol";
+import "../../utils/utils.sol";
+import "../../../src/EtherFiNodesManager.sol";
+import "../../../src/LiquidityPool.sol";
+import "../../../src/EtherFiOracle.sol";
+import "../../../src/EtherFiAdmin.sol";
+import "../../../src/RoleRegistry.sol";
+import "../../../test/common/ArrayTestHelper.sol";
+import "../../deploys/Deployed.s.sol";
+import "../../../src/WithdrawRequestNFT.sol";
+
+/**
+Transactions:
+1. Set validator size to 2000 ether
+2. AVS Operators submit the report
+3. Admin executes the tasks using that report
+4. Admin executes the validator approval task
+
+OR ANOTHER WAY:
+
+1. Set validator size to 2000 ether
+2. Give the LIQUIDITY_POOL_VALIDATOR_APPROVER_ROLE to the ETHERFI_OPERATING_ADMIN
+3. Call batchApproveRegistration() to approve the validator by the ETHERFI_OPERATING_ADMIN
+4. Remove the LIQUIDITY_POOL_VALIDATOR_APPROVER_ROLE from the ETHERFI_OPERATING_ADMIN
+ */
+
+contract TopUpFork is Script, Deployed, Utils, ArrayTestHelper {
+    int256 slotsPerEpoch = 32;
+    int256 secondsPerSlot = 12;
+
+    EtherFiNodesManager constant etherFiNodesManager = EtherFiNodesManager(payable(ETHERFI_NODES_MANAGER));
+    RoleRegistry constant roleRegistry = RoleRegistry(ROLE_REGISTRY);
+    LiquidityPool constant liquidityPool = LiquidityPool(payable(LIQUIDITY_POOL));
+    EtherFiOracle constant etherFiOracleInstance = EtherFiOracle(payable(ETHERFI_ORACLE));
+    EtherFiAdmin constant etherFiAdminInstance = EtherFiAdmin(payable(ETHERFI_ADMIN));
+    WithdrawRequestNFT constant withdrawRequestNFTInstance = WithdrawRequestNFT(payable(WITHDRAW_REQUEST_NFT));
+    address constant NODE_ADDRESS = 0xfbD914e11dF3DB8f475ae9C36ED46eE0c48f6B79;
+    address constant AVS_OPERATOR_1 = 0xDd777e5158Cb11DB71B4AF93C75A96eA11A2A615;
+    address constant AVS_OPERATOR_2 = 0x2c7cB7d5dC4aF9caEE654553a144C76F10D4b320;
+
+    uint256 constant BID_ID = 110766;
+    bytes constant PUBKEY = hex"a538a38970260348b6258eec086b932a76d369c96b5c87de5645807657c6128312e0c76bcd9987469ffe16d425bc971e";
+    // provide 96 bytes of zeros for signature
+    bytes constant SIGNATURE = hex"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+    function run() external {
+        console2.log("=== TOP UP FORK ===");
+
+        vm.prank(OPERATING_TIMELOCK);
+        liquidityPool.setValidatorSizeWei(2000 ether);
+
+        IEtherFiOracle.OracleReport memory report;
+        uint256[] memory emptyVals = new uint256[](0);
+        report = IEtherFiOracle.OracleReport(
+            etherFiOracleInstance.consensusVersion(), 0, 0, 0, 0, 0, 0, emptyVals, emptyVals, 0, 0
+        );
+
+        (report.refSlotFrom, report.refSlotTo, report.refBlockFrom) = etherFiOracleInstance.blockStampForNextReport();
+
+        report.validatorsToApprove = new uint256[](1);
+        report.validatorsToApprove[0] = BID_ID;
+        report.lastFinalizedWithdrawalRequestId = withdrawRequestNFTInstance.lastFinalizedRequestId();
+
+        // Need to advance time so that the report epoch is finalized
+        // Report epoch must be at least 2 epochs behind current epoch (i.e., current epoch >= report epoch + 3)
+        uint32 minCurrentSlot = ((report.refSlotTo / 32) + 3) * 32;
+        uint32 currentSlot = etherFiOracleInstance.computeSlotAtTimestamp(block.timestamp);
+        uint256 slotsToAdvance = minCurrentSlot > currentSlot ? minCurrentSlot - currentSlot : 100;
+        
+        vm.roll(block.number + slotsToAdvance);
+        vm.warp(etherFiOracleInstance.beaconGenesisTimestamp() + 12 * (currentSlot + slotsToAdvance));
+        
+        // Set refBlockTo to a block number that is < block.number and > lastAdminExecutionBlock
+        report.refBlockTo = uint32(block.number - 1);
+        if (report.refBlockTo <= etherFiAdminInstance.lastAdminExecutionBlock()) {
+            report.refBlockTo = etherFiAdminInstance.lastAdminExecutionBlock() + 1;
+        }
+
+        vm.prank(AVS_OPERATOR_1);
+        etherFiOracleInstance.submitReport(report);
+
+        vm.prank(AVS_OPERATOR_2);
+        etherFiOracleInstance.submitReport(report);
+
+        // Advance time for postReportWaitTimeInSlots
+        uint256 slotsToWait = uint256(etherFiAdminInstance.postReportWaitTimeInSlots() + 1);
+        uint32 slotAfterReport = etherFiOracleInstance.computeSlotAtTimestamp(block.timestamp);
+        vm.roll(block.number + slotsToWait);
+        vm.warp(etherFiOracleInstance.beaconGenesisTimestamp() + 12 * (slotAfterReport + slotsToWait));
+
+        vm.prank(ADMIN_EOA);
+        etherFiAdminInstance.executeTasks(report);
+
+        bytes32 reportHash = etherFiOracleInstance.generateReportHash(report);
+        bytes32 taskHash = keccak256(abi.encode(reportHash, report.validatorsToApprove));
+        (bool completed, bool exists) = etherFiAdminInstance.validatorApprovalTaskStatus(taskHash);
+
+        bytes[] memory pubKeys = new bytes[](1);
+        bytes[] memory signatures = new bytes[](1);
+        pubKeys[0] = PUBKEY;
+        signatures[0] = SIGNATURE;
+
+        vm.prank(ADMIN_EOA);
+        try etherFiAdminInstance.executeValidatorApprovalTask(reportHash, report.validatorsToApprove, pubKeys, signatures) {
+            (completed, exists) = etherFiAdminInstance.validatorApprovalTaskStatus(taskHash);
+        } catch {
+            console.log("error");
+        }
+    }
+}
