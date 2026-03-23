860f2aeafae6cb45d2d0a172b1c660f4e413a153
pankajjagtapp <pankaj@ether.fi>
2026-03-23 14:55:56 -0500

    feat: add EtherFiRedemptionManager deployment and upgrade logic to RestakerRoles, update gas stipend for ETH transfers

--- script/upgrades/restaker-roles/deploy.s.sol
+++ script/upgrades/restaker-roles/deploy.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.27;
 
 import "forge-std/Script.sol";
 import {EtherFiRestaker} from "../../../src/EtherFiRestaker.sol";
+import {EtherFiRedemptionManager} from "../../../src/EtherFiRedemptionManager.sol";
 import {Utils} from "../../utils/utils.sol";
 
 /**
@@ -16,9 +17,10 @@ import {Utils} from "../../utils/utils.sol";
  */
 contract DeployEtherFiRestakerWithRoles is Utils {
     address public etherFiRestakerImpl;
+    address public redemptionManagerImpl;
 
-    // Salt derived from a short description of this change
-    bytes32 commitHashSalt = keccak256("restaker-roles-v1");
+    // Salt derived from a short description of this change. to update this post final audit
+    bytes32 commitHashSalt = keccak256("restaker-roles-v2");
 
     function run() public {
         console2.log("================================================");
@@ -40,11 +42,29 @@ contract DeployEtherFiRestakerWithRoles is Utils {
             );
             etherFiRestakerImpl = deploy(contractName, constructorArgs, bytecode, commitHashSalt, true, mainnetCreate2Factory);
         }
+        {
+            string memory contractName = "EtherFiRedemptionManager";
+            bytes memory constructorArgs = abi.encode(
+                LIQUIDITY_POOL,
+                EETH,
+                WEETH,
+                WITHDRAW_REQUEST_NFT_BUYBACK_SAFE,
+                ROLE_REGISTRY,
+                ETHERFI_RESTAKER,
+                PRIORITY_WITHDRAWAL_QUEUE
+            );
+            bytes memory bytecode = abi.encodePacked(
+                type(EtherFiRedemptionManager).creationCode,
+                constructorArgs
+            );
+            redemptionManagerImpl = deploy(contractName, constructorArgs, bytecode, commitHashSalt, true, mainnetCreate2Factory);
+        }
 
         vm.stopBroadcast();
 
         console2.log("");
         console2.log("=== Deployment Summary ===");
+        console2.log("EtherFiRedemptionManager Implementation:", redemptionManagerImpl);
         console2.log("EtherFiRestaker Implementation:", etherFiRestakerImpl);
     }
 }
--- script/upgrades/restaker-roles/transactions.s.sol
+++ script/upgrades/restaker-roles/transactions.s.sol
@@ -6,6 +6,7 @@ import "forge-std/console2.sol";
 import {Utils} from "../../utils/utils.sol";
 import {EtherFiTimelock} from "../../../src/EtherFiTimelock.sol";
 import {EtherFiRestaker} from "../../../src/EtherFiRestaker.sol";
+import {EtherFiRedemptionManager} from "../../../src/EtherFiRedemptionManager.sol";
 import {IRoleRegistry} from "../../../src/interfaces/IRoleRegistry.sol";
 import {IEtherFiRateLimiter} from "../../../src/interfaces/IEtherFiRateLimiter.sol";
 import {ContractCodeChecker} from "../../../script/ContractCodeChecker.sol";
@@ -13,10 +14,11 @@ import "@openzeppelin-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 /**
  * @title RestakerRolesTransactions
- * @notice Schedules and executes the EtherFiRestaker upgrade that introduces per-function roles,
- *         then grants the new roles to ETHERFI_OPERATING_ADMIN
+ * @notice Schedules and executes two upgrades in a single timelock batch:
+ *         1. EtherFiRestaker: introduces per-function RoleRegistry roles
+ *         2. EtherFiRedemptionManager: EIP-7702 gas fix (10k -> 100k gas stipend)
  *
- * New roles (grant via RoleRegistry after upgrade):
+ * New Restaker roles (grant via RoleRegistry after upgrade):
  *   ETHERFI_RESTAKER_STETH_CLAIM_WITHDRAWALS_ROLE    -> stEthClaimWithdrawals
  *   ETHERFI_RESTAKER_STETH_REQUEST_WITHDRAWAL_ROLE   -> stEthRequestWithdrawal
  *   ETHERFI_RESTAKER_QUEUE_WITHDRAWALS_ROLE          -> queueWithdrawals
@@ -31,6 +33,8 @@ contract RestakerRolesTransactions is Utils {
 
     // TODO: fill in after running deploy.s.sol
     address constant etherFiRestakerImpl = 0x2d09A6561588506aF434CEe87Eac16Aba09d5641;
+    // TODO: fill in after running redemption-manager-7702/deploy.s.sol
+    address constant redemptionManagerImpl = address(0);
 
     bytes32 constant commitHashSalt = keccak256("restaker-roles-v1"); // TODO: fill in after audit
 
@@ -40,15 +44,27 @@ contract RestakerRolesTransactions is Utils {
     uint64 constant RATE_LIMIT_CAPACITY = 100_000_000_000_000;
     uint64 constant RATE_LIMIT_REFILL_RATE = 2_000_000_000;
 
+    //--------------------------------------------------------------------------------------
+    //---------------------------- IMMUTABLE SNAPSHOTS (PRE-UPGRADE) -----------------------
+    //--------------------------------------------------------------------------------------
+    ImmutableSnapshot internal preRestakerImmutables;
+    ImmutableSnapshot internal preRedemptionManagerImmutables;
+
+    //--------------------------------------------------------------------------------------
+    //---------------------------- ACCESS CONTROL SNAPSHOTS (PRE-UPGRADE) ------------------
+    //--------------------------------------------------------------------------------------
     address internal preRestakerOwner;
+    address internal preRedemptionManagerOwner;
+    bool internal preRedemptionManagerPaused;
 
     function run() public {
         console2.log("================================================");
-        console2.log("=== EtherFiRestaker Roles Upgrade ==============");
+        console2.log("=== Restaker Roles + RedemptionManager 7702 ====");
         console2.log("================================================");
         console2.log("");
 
         require(etherFiRestakerImpl != address(0), "Set etherFiRestakerImpl before running");
+        require(redemptionManagerImpl != address(0), "Set redemptionManagerImpl before running");
 
         contractCodeChecker = new ContractCodeChecker();
 
@@ -57,6 +73,7 @@ contract RestakerRolesTransactions is Utils {
         executeUpgrade();
         setUpRateLimiters();
         verifyUpgrade();
+        verifyImmutablePreservation();
         verifyAccessControlPreservation();
 
         console2.log("=== Upgrade Complete ===");
@@ -68,16 +85,48 @@ contract RestakerRolesTransactions is Utils {
     function verifyDeployedBytecode() public {
         console2.log("=== Verifying Deployed Bytecode ===");
 
-        EtherFiRestaker expected = new EtherFiRestaker(
+        EtherFiRestaker expectedRestaker = new EtherFiRestaker(
             address(EIGENLAYER_REWARDS_COORDINATOR),
             address(ETHERFI_REDEMPTION_MANAGER),
             address(ROLE_REGISTRY),
             address(ETHERFI_RATE_LIMITER)
         );
-        contractCodeChecker.verifyContractByteCodeMatch(etherFiRestakerImpl, address(expected));
+        contractCodeChecker.verifyContractByteCodeMatch(etherFiRestakerImpl, address(expectedRestaker));
+        console2.log("EtherFiRestaker bytecode verified!");
 
-        console2.log("Bytecode verification passed!");
+        EtherFiRedemptionManager expectedRedemption = new EtherFiRedemptionManager(
+            LIQUIDITY_POOL,
+            EETH,
+            WEETH,
+            TREASURY,
+            ROLE_REGISTRY,
+            ETHERFI_RESTAKER,
+            PRIORITY_WITHDRAWAL_QUEUE
+        );
+        contractCodeChecker.verifyContractByteCodeMatch(redemptionManagerImpl, address(expectedRedemption));
+        console2.log("EtherFiRedemptionManager bytecode verified!");
+
         console2.log("================================================");
+    }
+
+    //--------------------------------------------------------------------------------------
+    //------------------------------- IMMUTABLE SELECTOR DEFINITIONS -----------------------
+    //--------------------------------------------------------------------------------------
+    function getRestakerImmutableSelectors() internal pure returns (bytes4[] memory selectors) {
+        selectors = new bytes4[](2);
+        selectors[0] = bytes4(keccak256("rewardsCoordinator()"));
+        selectors[1] = bytes4(keccak256("etherFiRedemptionManager()"));
+    }
+
+    function getRedemptionManagerImmutableSelectors() internal pure returns (bytes4[] memory selectors) {
+        selectors = new bytes4[](7);
+        selectors[0] = bytes4(keccak256("roleRegistry()"));
+        selectors[1] = bytes4(keccak256("treasury()"));
+        selectors[2] = bytes4(keccak256("eEth()"));
+        selectors[3] = bytes4(keccak256("weEth()"));
+        selectors[4] = bytes4(keccak256("liquidityPool()"));
+        selectors[5] = bytes4(keccak256("etherFiRestaker()"));
+        selectors[6] = bytes4(keccak256("lido()"));
     }
 
     //--------------------------------------------------------------------------------------
@@ -85,10 +134,37 @@ contract RestakerRolesTransactions is Utils {
     //--------------------------------------------------------------------------------------
     function takePreUpgradeSnapshots() internal {
         console2.log("=== Taking Pre-Upgrade Snapshots ===");
+        console2.log("");
+
+        // Immutable snapshots
+        console2.log("--- Immutable Snapshots ---");
+        preRestakerImmutables = takeImmutableSnapshot(
+            ETHERFI_RESTAKER,
+            getRestakerImmutableSelectors()
+        );
+        console2.log("  EtherFiRestaker: captured", preRestakerImmutables.selectors.length, "immutables");
+
+        preRedemptionManagerImmutables = takeImmutableSnapshot(
+            ETHERFI_REDEMPTION_MANAGER,
+            getRedemptionManagerImmutableSelectors()
+        );
+        console2.log("  EtherFiRedemptionManager: captured", preRedemptionManagerImmutables.selectors.length, "immutables");
+
+        // Access control snapshots
+        console2.log("");
+        console2.log("--- Access Control Snapshots ---");
 
         preRestakerOwner = _getOwner(ETHERFI_RESTAKER);
         console2.log("  EtherFiRestaker owner:", preRestakerOwner);
 
+        preRedemptionManagerOwner = _getOwner(ETHERFI_REDEMPTION_MANAGER);
+        console2.log("  EtherFiRedemptionManager owner:", preRedemptionManagerOwner);
+
+        preRedemptionManagerPaused = _getPaused(ETHERFI_REDEMPTION_MANAGER);
+        console2.log("  EtherFiRedemptionManager paused:", preRedemptionManagerPaused);
+
+        console2.log("");
+        console2.log("Pre-upgrade snapshots captured!");
         console2.log("================================================");
     }
 
@@ -104,22 +180,27 @@ contract RestakerRolesTransactions is Utils {
         bytes32 completeRole = keccak256("ETHERFI_RESTAKER_COMPLETE_QUEUED_WITHDRAWALS_ROLE");
         bytes32 depositRole = keccak256("ETHERFI_RESTAKER_DEPOSIT_INTO_STRATEGY_ROLE");
 
-        address[] memory targets = new address[](6);
+        address[] memory targets = new address[](7);
         bytes[] memory data = new bytes[](targets.length);
         uint256[] memory values = new uint256[](targets.length);
 
+        // Restaker upgrade
         targets[0] = ETHERFI_RESTAKER;
         data[0] = abi.encodeWithSelector(UUPSUpgradeable.upgradeTo.selector, etherFiRestakerImpl);
+        // Restaker role grants
         targets[1] = ROLE_REGISTRY;
-        data[1] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, claimRole, ETHERFI_OPERATING_ADMIN);
+        data[1] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, claimRole, ADMIN_EOA);
         targets[2] = ROLE_REGISTRY;
-        data[2] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, requestRole, ETHERFI_OPERATING_ADMIN);
+        data[2] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, requestRole, ADMIN_EOA);
         targets[3] = ROLE_REGISTRY;
-        data[3] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, queueRole, ETHERFI_OPERATING_ADMIN);
+        data[3] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, queueRole, ADMIN_EOA);
         targets[4] = ROLE_REGISTRY;
-        data[4] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, completeRole, ETHERFI_OPERATING_ADMIN);
+        data[4] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, completeRole, ADMIN_EOA);
         targets[5] = ROLE_REGISTRY;
-        data[5] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, depositRole, ETHERFI_OPERATING_ADMIN);
+        data[5] = abi.encodeWithSelector(IRoleRegistry.grantRole.selector, depositRole, ADMIN_EOA);
+        // RedemptionManager upgrade (EIP-7702 gas fix)
+        targets[6] = ETHERFI_REDEMPTION_MANAGER;
+        data[6] = abi.encodeWithSelector(UUPSUpgradeable.upgradeTo.selector, redemptionManagerImpl);
 
         bytes32 timelockSalt = keccak256(abi.encode(targets, data, commitHashSalt, block.number));
 
@@ -241,9 +322,15 @@ contract RestakerRolesTransactions is Utils {
     function verifyUpgrade() public view {
         console2.log("=== Verifying Upgrade ===");
 
+        // Verify EtherFiRestaker upgrade
         address currentImpl = getImplementation(ETHERFI_RESTAKER);
         require(currentImpl == etherFiRestakerImpl, "EtherFiRestaker upgrade failed");
         console2.log("EtherFiRestaker implementation:", currentImpl);
+
+        // Verify EtherFiRedemptionManager upgrade
+        address currentRedemptionImpl = getImplementation(ETHERFI_REDEMPTION_MANAGER);
+        require(currentRedemptionImpl == redemptionManagerImpl, "EtherFiRedemptionManager upgrade failed");
+        console2.log("EtherFiRedemptionManager implementation:", currentRedemptionImpl);
 
         // Verify new role constants are accessible
         EtherFiRestaker restaker = EtherFiRestaker(payable(ETHERFI_RESTAKER));
@@ -291,18 +378,64 @@ contract RestakerRolesTransactions is Utils {
     }
 
     //--------------------------------------------------------------------------------------
+    //------------------------------- IMMUTABLE PRESERVATION VERIFICATION -----------------
+    //--------------------------------------------------------------------------------------
+    function verifyImmutablePreservation() internal view {
+        console2.log("=== Verifying Immutable Preservation ===");
+        console2.log("");
+
+        ImmutableSnapshot memory postRestakerImmutables = takeImmutableSnapshot(
+            ETHERFI_RESTAKER,
+            getRestakerImmutableSelectors()
+        );
+        verifyImmutablesUnchanged(preRestakerImmutables, postRestakerImmutables, "EtherFiRestaker");
+
+        ImmutableSnapshot memory postRedemptionManagerImmutables = takeImmutableSnapshot(
+            ETHERFI_REDEMPTION_MANAGER,
+            getRedemptionManagerImmutableSelectors()
+        );
+        verifyImmutablesUnchanged(preRedemptionManagerImmutables, postRedemptionManagerImmutables, "EtherFiRedemptionManager");
+
+        console2.log("");
+        console2.log("All immutable preservation checks passed!");
+        console2.log("================================================");
+    }
+
+    //--------------------------------------------------------------------------------------
     //------------------------------- ACCESS CONTROL PRESERVATION --------------------------
     //--------------------------------------------------------------------------------------
     function verifyAccessControlPreservation() internal view {
         console2.log("=== Verifying Access Control Preservation ===");
+        console2.log("");
+
+        // Owner verification
+        console2.log("--- Owner Verification ---");
 
         address postOwner = _getOwner(ETHERFI_RESTAKER);
         require(postOwner == preRestakerOwner, "EtherFiRestaker: owner changed");
         console2.log("[OWNER OK] EtherFiRestaker:", postOwner);
 
-        verifyNotReinitializable(ETHERFI_RESTAKER, "EtherFiRestaker");
+        address postRedemptionOwner = _getOwner(ETHERFI_REDEMPTION_MANAGER);
+        require(postRedemptionOwner == preRedemptionManagerOwner, "EtherFiRedemptionManager: owner changed");
+        console2.log("[OWNER OK] EtherFiRedemptionManager:", postRedemptionOwner);
 
-        console2.log("Access control preservation checks passed!");
+        // Paused state verification
+        console2.log("");
+        console2.log("--- Paused State Verification ---");
+
+        bool postRedemptionManagerPaused = _getPaused(ETHERFI_REDEMPTION_MANAGER);
+        require(postRedemptionManagerPaused == preRedemptionManagerPaused, "EtherFiRedemptionManager: paused state changed");
+        console2.log("[PAUSED OK] EtherFiRedemptionManager:", postRedemptionManagerPaused);
+
+        // Initialization state verification
+        console2.log("");
+        console2.log("--- Initialization State Verification ---");
+
+        verifyNotReinitializable(ETHERFI_RESTAKER, "EtherFiRestaker");
+        verifyNotReinitializable(ETHERFI_REDEMPTION_MANAGER, "EtherFiRedemptionManager");
+
+        console2.log("");
+        console2.log("All access control preservation checks passed!");
         console2.log("================================================");
     }
 }
