189f19ece45175bd08640ba1d2e2ef905e66c0d6
Yash Saraswat <yash@ether.fi>
2026-05-29 16:30:00 -0500

    refcator: reorder and group Liquifier and add natspec

--- src/deposits/interfaces/ILiquifier.sol
+++ src/deposits/interfaces/ILiquifier.sol
@@ -154,6 +154,18 @@ interface ILiquifier {
         bool isL2Eth;
     }
 
+    struct ConstructorAddresses {
+        address liquidityPool;
+        address lidoWithdrawalQueue;
+        address lido;
+        address stEth_Eth_Pool;
+        address roleRegistry;
+        address stEthPriceFeed;
+        address blacklister;
+        address etherfiRestaker;
+        address l1SyncPool;
+    }
+
     function lido() external view returns (ILido);
     function lidoWithdrawalQueue() external view returns (ILidoWithdrawalQueue);
     function tokenInfos(address _token) external view returns (uint128, uint128, IStrategy, bool, uint16, uint32, uint32, uint32, uint96, uint96, bool);
